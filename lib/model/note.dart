5c38f8762439c717ec769003eb88fa3afdd81b62
michalnowak18 <michalkarol.nowak2001@gmail.com>
2022-11-12 23:03:13 +0100

    Removed unused field.

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,48 +1,43 @@
+const String notesTableName = 'notes';
+
 class NoteFields {
-  static final List<String> values = [id, number, title, content];
+  static final List<String> values = [id, title, content];
 
   static const String id = '_id';
-  static const String number = 'number';
   static const String title = 'title';
   static const String content = 'content';
 }
 
 class Note {
   final int? id;
-  final int number;
   final String title;
   final String content;
 
   const Note({
     this.id,
-    required this.number,
     required this.title,
     required this.content,
   });
 
   Note copy({
     int? id,
-    int? number,
     String? title,
     String? content,
   }) =>
       Note(
         id: id ?? this.id,
-        number: number ?? this.number,
         title: title ?? this.title,
         content: content ?? this.content,
       );
 
   static Note fromJson(Map<String, Object?> json) => Note(
         id: json[NoteFields.id] as int?,
-        number: json[NoteFields.number] as int,
         title: json[NoteFields.title] as String,
         content: json[NoteFields.content] as String,
       );
 
   Map<String, Object?> toJson() => {
         NoteFields.id: id,
-        NoteFields.number: number,
         NoteFields.title: title,
         NoteFields.content: content,
       };
